557749ec456a446e06b09c48efe3cf918cbbd31c
Stanislav <pavlov-sta@mail.ru>
2019-02-08 10:24:52 +0500

    Первое задание диплома

---
+++ bjs-diplom/static/main.js
@@ -0,0 +1,48 @@
+class Profile {
+  
+  create(
+    {
+      username,
+      name: { firstName, lastName },
+      password,
+    },
+    callback
+  ) {
+    return ApiConnector.createUser({ username, name: { firstName, lastName }, password }, (err, data) => {
+      console.log(`${firstNam} , ${lastName} `);
+      callback(err, data);
+    });
+  }
+
+  performLogin({ username, password }, callback) {
+    return ApiConnector.performLogin({ username, password }, (err, data) => {
+      callback(err, data);
+    });
+  }
+
+  addMoney({ currency, amount }, callback) {
+    return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+      console.log(`Добавим ${amount} из ${currency} в ${this.username}`);
+      callback(err, data);
+    });
+  }
+
+  transferMoney({ to, amount }, callback) {
+    return ApiConnector.transferMoney({ to, amount }, (err, data) => {
+      callback(err, data);
+    });
+  }
+
+  convertMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
+    return ApiConnector.convertMoney({ fromCurrency, targetCurrency, targetAmount }, (err, data) => {
+      callback(err, data);
+    });
+  }
+
+  getStocks(callback) {
+    return ApiConnector.getStocks((err, data) => {
+      callback(err, data);
+    });
+  }
+}
+
